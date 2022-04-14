ca7bcb32c6fc665c21167bae16a8f1ed7cfdae4d
bega <60026940+ed17usiv@users.noreply.github.com>
2022-04-14 08:22:23 +0200

    dbm

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : 'postgresql-corrugated-72903',
-    user : 'bega',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
